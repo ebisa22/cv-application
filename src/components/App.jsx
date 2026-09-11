4acf3868a2f0f279c77bb6706a28e0055de8ba35
ebisa <225586468+ebisa22@users.noreply.github.com>
2026-09-11 18:53:19 +0300

    Add mobile responsive layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import Footer from './Footer.jsx'
 import { clearForm } from './RegisterFunction.js'
 import '../styles/App.css'
 function App() {
-  const [appMode,setAppMode]=useState("main");
+  const [appMode,setAppMode]=useState("home");
   function goHome(){
     setAppMode('home')
   }
